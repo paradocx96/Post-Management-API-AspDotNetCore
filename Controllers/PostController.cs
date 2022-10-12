40368c27ae3551a5f6212ca5fabf9a004b853926
Navinda Chandrasiri <adilanga@gmail.com>
2022-10-12 08:38:19 +0530

    Controller Impl

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PostWebAPI.Models;
+using PostWebAPI.Services;
 
 namespace PostWebAPI.Controllers
 {
