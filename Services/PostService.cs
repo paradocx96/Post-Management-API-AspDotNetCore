002af0a7f488631462d34126f0c88be725b7229b
Navinda Chandrasiri <adilanga@gmail.com>
2022-10-12 08:38:08 +0530

    Service Impl

--- Services/PostService.cs
+++ Services/PostService.cs
@@ -1,6 +1,44 @@
-﻿namespace PostWebAPI.Services;
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using PostWebAPI.Models;
+
+namespace PostWebAPI.Services;
 
 public class PostService
 {
-    
+    private readonly IMongoCollection<Post> _postsCollection;
+
+    // Database Configuration
+    public PostService(
+        IOptions<PostDatabaseSettings> postDatabaseSettings)
+    {
+        var mongoClient = new MongoClient(
+            postDatabaseSettings.Value.ConnectionString);
+
+        var mongoDatabase = mongoClient.GetDatabase(
+            postDatabaseSettings.Value.DatabaseName);
+
+        _postsCollection = mongoDatabase.GetCollection<Post>(
+            postDatabaseSettings.Value.CollectionName);
+    }
+
+    // Get Posts
+    public async Task<List<Post>> GetAsync() =>
+        await _postsCollection.Find(_ => true).ToListAsync();
+
+    // Get Post By Id
+    public async Task<Post?> GetAsync(string id) =>
+        await _postsCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+    // Create Post
+    public async Task CreateAsync(Post newPost) =>
+        await _postsCollection.InsertOneAsync(newPost);
+
+    // Update Post
+    public async Task UpdateAsync(string id, Post updatePost) =>
+        await _postsCollection.ReplaceOneAsync(x => x.Id == id, updatePost);
+
+    // Delete Post
+    public async Task RemoveAsync(string id) =>
+        await _postsCollection.DeleteOneAsync(x => x.Id == id);
 }
